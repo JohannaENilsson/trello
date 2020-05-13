d2ce7866f96afc8418a88c4cd7e2b88a82f48d82
Johanna Nilsson <nijodotcom@gmail.com>
2020-05-13 14:54:38 +0200

    can get list from server

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
 import './App.css';
 
 function App() {
+  const [newTodo, setNewTodo] = useState('Add todo');
+  const [newList, setNewList] = useState('New list');
+  const [newListName, setNewListName] = useState('List name')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:8090/lists')
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  function addTodo(e) {
+    e.preventDefault();
+    console.log(newTodo);
+  }
+
+  function createNewList(e) {
+    e.preventDefault();
+    console.log(newList);
+  }
+
+  function changeListName(e) {
+    e.preventDefault();
+    console.log(newList);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <header>Organize</header>
+      <main className='board'>
+        <section className='listContainer'>
+          I´m a list container
+          <form onClick={(e) => changeListName(e)}>
+          <input type='text' value={newListName} onChange={(e) => setNewListName(e.target.value)}/>
+          <button>Change name</button>
+          </form>
+          <ul>
+            <li>Item / Todo</li>
+            <li>Item / Todo</li>
+            <li>Item / Todo / Item / Todo / Item / Todo</li>
+          </ul>
+          <form onClick={(e) => addTodo(e)} >
+            <input
+              type='text'
+              value={newTodo}
+              onChange={(e) => setNewTodo(e.target.value)}
+            />
+            <button>Add todo</button>
+          </form>
+        </section>
+
+        <section className='listContainer'>
+          Create a new list
+          <form onClick={(e) => createNewList(e)}>
+            <input
+              type='text'
+              value={newList}
+              onChange={(e) => setNewList(e.target.value)}
+            />
+            <button>Add new list</button>
+          </form>
+        </section>
+      </main>
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -5,59 +5,110 @@ const app = express();
 // list -> obj {name: listName, items: [{title: str, description: str, created: date}]}
 // item -> obj {title: str, description: str, created: date}
 
-const {getClient, getDB, createObjectId} = require('./db');
-
-
-
-
 // middleware to parse data
-// middleware log -> req, res, method, answer time
 
+let lists = [
+  {
+    id: 1,
+    listName: 'Todo',
+  },
+  {
+    id: 2,
+    listName: 'Inprogress',
+  },
+];
 
-// app.get('/board', (req, res) => {
-//   const db = getDB();
-//   db.collection('board')
-//     .find({})
-//     .toArray()
-//     .then((data) => {
-//       console.log('Data from server ', data);
-//       res.status(200).send(data);
-//     })
-//     .catch((err) => {
-//       console.error(err);
-//       res.status(500).end();
-//     });
-// });
-// app.use(express.json());
+let items = [
+  {
+    id: 001,
+    name: 'Cook food',
+    description: 'make som delicious vegan food',
+    data: 'create time',
+  },
+  {
+    id: 002,
+    name: 'Watch movie',
+    description: 'Se all Anthony Hopkins movies',
+    data: 'create time',
+  },
+  {
+    id: 003,
+    name: 'Pet the cat',
+    description: 'Give her some love',
+    data: 'create time',
+  },
+  {
+    id: 004,
+    name: 'Code',
+    description: 'Learn how to code',
+    data: 'create time',
+  },
+];
 
+// {listId: String, itemId :string}
+let relationListItem = [];
 
-app.get('/board', (req, res) => {
-    // res.send('some other tetxs')
-    res.send({test: 'another test'})
+// {boardID: string, listID: string} ???
+// Kan hämta ut dem som är kopplade till boarden och därefter itemsen som är kopplade till resp lista
+let board = [];
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  let start = new Date();
+  res.once('finish', () => {
+    console.log(req.method, req.path, res.statusCode, new Date() - start, 'ms');
   });
+  next();
+});
 
-app.get('/board/:id', (req, res) => {
+app.use((req, res, next) => {
+  let data = req.body;
+  if (!data) {
+    res.status(400).end();
+    return;
+  }
+  next();
+});
+
+
+let listRouter = express.Router();
+listRouter.get('/', (req, res) => {
+  res.status(200).send(lists);
+});
+
+listRouter.get('/:id', (req, res) => {
+    let id = parseInt(req.params.id);
+    let list = lists.find( function(list){
+        return list.id === id;
+    });
+// Kollar så det finns någon id som matchar
+    if(list){
+        res.status(200).send(list);
+    } else {
+        res.status(404).end();
+        return; 
+    }
+});
+
+listRouter.post('/', (req, res) => {
+    let data = req.body;
+    if(!data.listName){
+        res.status(400).end();
+        return;
+    }
     
-})
+});
 
-app.post('/board', (req, res) => {
-    
-})
+// listRouter.delete('/board/:id', (req, res) => {});
 
-app.delete('/board/:id', (req, res) => {
-    
-})
+// listRouter.get('/board', (req, res) => {});
 
-app.get('/board', (req, res) => {
-    
-})
+// listRouter.patch('/board/:id', (req, res) => {});
 
-app.patch('/board/:id', (req, res) => {
-    
-})
-
+app.use('/lists', listRouter);
 
 const PORT = 8090;
 app.listen(PORT, () => {
-    console.log(`Connected to server at ${PORT}`);
-})
+  console.log(`Connected to server at ${PORT}`);
+});
